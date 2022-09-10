3ba4af0aac8d904547f5395d904e21160edf4fdd
MarcAmbof <marcadoni25041@gmail.com>
2022-09-10 05:20:55 -0400

    configuration heroku effectuer

--- resources/views/Dashboard/procureur.blade.php
+++ resources/views/Dashboard/procureur.blade.php
@@ -10,6 +10,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="{{ asset('css/fichier_css/dataTables.bootstrap5.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/fichier_css/style.css') }}" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
     <title>Espace Procureur</title>
 </head>
 
@@ -144,6 +153,10 @@
                                                             href=""><span class="">
                                                                 <i class="bi bi-file-text-fill"></i>
                                                             </span></a></button>
+                                                    <button type="button" class="btn btn-primary"
+                                                        data-toggle="modal" data-target="#exampleModalCenter">
+                                                        Launch demo modal
+                                                    </button>
                                                     <button type="button" class="btn btn-danger"><a
                                                             style="color:black" class=" d-flex justify-content-center"
                                                             href=""><span class=""><i
@@ -166,6 +179,26 @@
                                 </table>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
                     </div>
                 </div>
             </div>
